aa9ccf9f3d3b733e9e09ac08f54fd7c41091bc6d
Daniil Genin <daniil.genin@post.runi.ac.il>
2024-01-19 00:53:33 +0300

    str

--- StringOps.java
+++ StringOps.java
@@ -22,24 +22,51 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
+        //System.out.println(capVowelsLowRest(null));
+        System.out.println(allIndexOf("Hello world",'l'));
     }
-
     public static String capVowelsLowRest (String string) {
         // Write your code here:
         String str = "";
         for (int i = 0; i < string.length(); i++) {
-            char currentChar = input.charAt(i);
+            char currentChar = string.charAt(i);
+            if (currentChar == 'a' || currentChar == 'e' || currentChar == 'o' || currentChar == 'u' || currentChar == 'i') {
+                str += (char) (currentChar - 'a' + 'A');
+            }   
+            else if (currentChar == 'A' || currentChar == 'E' || currentChar == 'O' || currentChar == 'U' || currentChar == 'I') {
+                str += currentChar;
+            }
+            else{
+                str += currentChar;
+            }
+
+            
+        }
         return str;
     }
 
     public static String camelCase (String string) {
         // Write your code here:
+
         return "";
     }
 
     public static int[] allIndexOf (String string, char chr) {
         // Write your code here:
-        return new int[1];
+        int count = 0;
+        for (int i = 0; i < string.length(); i++){
+            if (chr == string.charAt(i)){
+                count += 1;
+            }
+        }
+        int array[] = new int [count];
+        for (int i = 0; i < string.length(); i++){
+            for (int n = 0; n < array.length; n++){
+                if (chr == string.charAt(i)){
+                    array[n] = chr;
+                }
+            }
+        }
+        return array;
     }
 }
